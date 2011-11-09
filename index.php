5150310c9a6e25159f68c8854f91eeef2c5424aa
Jui-Nan Lin <jnlin@pixnet.tw>
2011-11-09 21:29:40 +0800

    add callback

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-<?= json_encode(array('ip' => $_SERVER['HTTP_X_FORWARDED_FOR'], 'address' => $_SERVER['HTTP_X_FORWARDED_FOR'])) ?>
+<?php 
+$out = json_encode(array('ip' => $_SERVER['HTTP_X_FORWARDED_FOR'], 'address' => $_SERVER['HTTP_X_FORWARDED_FOR']));
+if ($cb = $_GET['callback']) {
+    $out = sprintf('%s(%s);', $cb, $out); 
+    header('Content-Type: application/javascript');
+} else {
+    header('Content-Type: application/json');
+}
+echo $out;
+?>
